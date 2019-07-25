6e8f03a9fa1f8231db45257396b04fdc661e8231
sitiaish <sitiaishahaz@gmail.com>
2019-07-25 16:29:58 +0800

    env gallery updated

--- js/env.js
+++ js/env.js
@@ -6,6 +6,7 @@ var width_ = 600 - margin_.left - margin_.right,
 var chart = d3.select("body")
   .select(".environmentsection")
   .append("svg")
+  .lower()
   .attr("id", "envchart")
   .attr("width", width_ + margin_.left + margin_.right)
   .attr("height", height_ + margin_.top + margin_.bottom)
@@ -16,6 +17,8 @@ d3.json("env.json").then(function(data) {
   let decadeEnv = d3.nest()
       .key(d => d.decade)
       .entries(data)
+
+  console.log(decadeEnv)
 
 // appending the the step for each stack
     decadeEnv.forEach(decade => {
@@ -39,14 +42,13 @@ d3.json("env.json").then(function(data) {
         .range([10, width_])
 
 // listing out the legend values
-    var subcats = ["cleanliness", "dengue", "foodhygeine", "sars", "sustain", "toilet"]
+    var subcats = ["Cleanliness", "Dengue Prevention", "Food Hygiene", "Sars Education", "Sustainability", "Toilet Hygiene"]
     var envcolors = ["#10ac84", "#ee5253", "#ffbe76", "#8395a7", "#badc58", "#54a0ff"];
 
 // tooltip
 var div = d3.select("body").append("div") 
     .attr("class", "envtooltip")       
     .style("opacity", 0);
-
 
 //  draw the chart
     chart.selectAll("g.subcat")
@@ -62,25 +64,31 @@ var div = d3.select("body").append("div")
       .attr("width", 60)
       .attr('x', function(d) {return bandScaleEnv(d.decade);})
       .attr("y", d => yScale(d.startY))
-      .on("mouseover", function(d) {    
-          div.transition()    
-          .duration(200)    
-          .style("opacity", .9);    
-          div .html(d.count)  
-          .style("left", (d3.event.pageX + 10) + "px")   
-          .style("top", (d3.event.pageY - 28) + "px")
-          d3.select(this)
-          .attr("stroke","#009432")
-          .attr("stroke-width", 4)
-          })          
-      .on("mouseout", function(d) {   
-          div.transition()    
-          .duration(500)    
-          .style("opacity", 0)
-          d3.select(this)
-          .attr("stroke","none")
-          })
-      
+
+d3.selectAll(".envsubcat")
+    .on("mouseover.count", function(d) {    
+        div.transition()    
+        .duration(200)    
+        .style("opacity", .9);    
+        div .html(d.count)  
+        .style("left", (d3.event.pageX + 10) + "px")   
+        .style("top", (d3.event.pageY - 28) + "px")
+        d3.select(this)
+        .attr("stroke","#009432")
+        .attr("stroke-width", 4)
+        })          
+    .on("mouseout", function(d) {   
+        div.transition()    
+        .duration(500)    
+        .style("opacity", 0)
+        d3.select(this)
+        .attr("stroke","none")
+        })
+
+d3.selectAll(".envsubcat")
+  .on("mouseover.posters", d => vm.envGallery = d.imgfull)
+
+
 
 
 // flipping the chart because i am noob
@@ -115,3 +123,9 @@ let legendenv = chart.selectAll(".legend")
         .style("text-anchor", "start")
         .text(function(d, i) { return labelenv[i]; });
 });
+
+// decadeEnv[0].values[0].imgfull[0]
+
+// decadeEnv[0].key
+
+// decadeEnv[0].values[0].subcat
