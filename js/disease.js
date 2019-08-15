b40a6fbf9f8ec342b15c0af4941d0d551fa95592
sitiaish <sitiaishahaz@gmail.com>
2019-08-15 22:35:10 +0800

    latest

--- js/disease.js
+++ js/disease.js
@@ -1,9 +1,16 @@
 let disease = d3.select("#disease")
 		.append('svg')
 		.attr('width', 935)
-		.attr('height', 380)
+		.attr('height', 320)
 		.attr("id", "diseaseSVG")
 		.style("display", "block")
+
+let diseaseContent = d3.select("#disease")
+						.append("div")
+						.attr("class", "content")
+						.style("margin-top", "50px")
+						.append("p")
+						.attr("class", "disease-content")
 
 d3.json("health3.json").then(function(data) {
 
@@ -39,35 +46,83 @@ d3.json("health3.json").then(function(data) {
 		.enter()
 		.append('rect')
 		.attr('class', 'diseaserect')
+		.attr('typeofdisease', d => d.subsubcat)
 		.attr('width', 25)
 		.attr('height', 10)
 	 	.attr('x', d => xdisScale(d.year))
-	 	.attr('y', (d,i) => 360 -20 - (i * 15))
+	 	.attr('y', (d,i) => 300 -20 - (i * 15))
+	 	.attr("fill", "#34495e")
 	 	.attr("fill", function(d){
-     		return d.subsubcat === 'aids' ? "#ff7675" 
-     		: d.subsubcat === 'cancer' ? "#00cec9"
-     		: d.subsubcat === 'high blood pressure' ? "#00b894"
-     		: d.subsubcat === 'sars' ? "#0984e3"
-     		: d.subsubcat === 'measles' ? "#0984e3"
-     		: d.subsubcat === 'diabetes' ? "#fab1a0"
-     		: d.subsubcat === 'dengue' ? "#fab1a0"
-     		: d.subsubcat === 'heart attack' ? "#fab1a0"
-     		: d.subsubcat === 'hepatitis' ? "#fab1a0"
-     		: d.subsubcat === 'mental illness' ? "#fab1a0"
-     		: d.subsubcat === 'tuberculosis' ? "#fab1a0"
-     		: "#fdcb6e"})
+	 		return d.subsubcat === 'aids' ? "#747d8c" 
+	 		// : d.subsubcat === 'cancer' ? "#bdc3c7"
+	 		// : d.subsubcat === 'high blood pressure' ? "#95a5a6"
+	 		// : d.subsubcat === 'sars' ? "#7f8c8d"
+	 		// : d.subsubcat === 'measles' ? "#57606f"
+	 		: "#747d8c"})
+	 	.on("mouseover", d => healthPosterHover(d.imgfull))	
+	 	.on("mouseover.color", function(d){
+	 		d3.select(this)
+	 			.attr("stroke", "black")
+	 	.on("mouseover.annotate", annoatatedis(d.subsubcat))
+	 	.on("mouseout", null)
+	 	})			
+
 
 	disease.append('g')
 		.call(d3.axisBottom(xdisScale))
-		.attr('transform', "translate(15, 360)")
+		.attr('transform', "translate(15, 300)")
 		.attr("class", "yearAxis")
-//img 
-
-	// let cloud = d3.select("#disease")
-	// 	.append("img")
-	// 	.attr("class", "cloud")
-	// 	.attr("src", "./img/cloud.png")	
-	//     .style("z-index", 1)
-	//     .style("width", "300px")
 });
+
+function changeColor(disease, color, text) {
+	d3.selectAll('.diseaserect')
+    .transition()		
+    .duration(100)
+    .attr("fill", "#747d8c")
+
+    d3.selectAll('.diseaserect[typeofdisease="' + disease + '"]')
+    .transition()		
+    .duration(100)
+    .attr("fill", color)
+
+	diseaseContent.html(`<hr>${text}`)
+};
+
+function healthPosterHover(posterUrl) {
+
+	d3.select("#heathSide")
+		.style("display", "block")
+		.select("img")
+		.attr("src", posterUrl)
+		.style("display", "block")
+}
+
+function annoatatedis(subcat) {
+
+	const annotations = [
+	  {
+	    note: {
+	      title: subcat,
+	    },
+		x: 940,
+	    y: 200,
+	    dy: -50,
+	    dx: -90
+	  }
+	]
+
+	// Remove previous annotation
+	d3.select("#diseaseSVG")
+	  .selectAll(".annotations")
+	  .attr("opacity", 0)
+
+
+	// Add annotation to the chart
+	const makeAnnotations = d3.annotation()
+	  .annotations(annotations)
+	d3.select("#diseaseSVG")
+	  .append("g")
+	  .call(makeAnnotations)
+};
+
 
